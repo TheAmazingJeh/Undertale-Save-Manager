15cb4159285b17d36ae66d6a8efd600e34acbba9
theamazingjeh <joehbooth2004@gmail.com>
2023-11-26 14:21:48 +0000

    Add comments to basicFileFunctions.py

--- basicFileFunctions.py
+++ basicFileFunctions.py
@@ -1,5 +1,6 @@
 import os
-    
+
+# Opens a file and returns the contents as a string
 def open_file(file_name): 
     try:
         with open(file_name, 'r') as file:
@@ -7,11 +8,13 @@ def open_file(file_name):
     except FileNotFoundError:
         return ""
 
-def save_file(file_name, data): # Saves a file
+# Saves a file, overwriting the contents
+def save_file(file_name, data): 
     with open(file_name, 'w') as file:
         file.write(data)
 
-def try_to_delete(file_name): # Tries to delete a file, but doesn't error if it doesn't exist
+# Tries to delete a file, but doesn't error if it doesn't exist
+def try_to_delete(file_name):
     try:
         os.remove(file_name)
     except FileNotFoundError:
